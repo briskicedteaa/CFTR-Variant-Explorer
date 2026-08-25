8641cf126a2dcad70ea1bb3011cba7eb00e7a17a
ericka <kkikipie@gmail.com>
2026-08-24 22:26:19 -0400

    Update variant_explorer.py

--- src/variant_explorer.py
+++ src/variant_explorer.py
@@ -1,10 +1,10 @@
 from .data_loader import load_data
 
 def get_position(position):
-    position_df, variants_df = load_data()
+    FinalPosition, CFTR_variants = load_data()
   
-    result = position_df[
-        position_df["Human_Position"] == position
+    result = FinalPosition[
+        FinalPosition["Human_Position"] == position
     ].copy()
 
     return result
