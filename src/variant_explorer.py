fd6a9d56740046841f5ddc2ba44d9a08f1001b4a
ericka <kkikipie@gmail.com>
2026-08-24 22:22:13 -0400

    Add get_position function to filter by Human_Position

--- src/variant_explorer.py
+++ src/variant_explorer.py
@@ -1,1 +1,10 @@
+from .data_loader import load_data
 
+def get_position(position):
+    position_df, variants_df = load_data()
+  
+    result = position_df[
+        position_df["Human_Position"] == position
+    ].copy()
+
+    return result
