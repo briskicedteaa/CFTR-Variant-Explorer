ee354471d5f33ba4b3dcd282ae97a181c8f95794
ericka <kkikipie@gmail.com>
2026-08-26 14:29:25 -0400

    Fixed small syntax error

--- src/app.py
+++ src/app.py
@@ -111,7 +111,7 @@ if st.button("Explore position"):
 
         variants = variants_df[variants_df["Position"] == 1481]
         
-    else position_info is not None: 
+    elif position_info is not None: 
         st.subheader(f"CFTR Position {position}")
 
         info = position_info.iloc[0]
