8b0327c5b361cd8a25cf68152d1168f5d2da180f
ericka <kkikipie@gmail.com>
2026-08-26 21:20:15 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -136,7 +136,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-N_PATH = Path(__file__).resolve().parent.parent / "images" / images/ "Untitled16_20260826211636.png"
+N_PATH = Path(__file__).resolve().parent.parent / "images" / "Untitled16_20260826211636.png"
 
 col1, col2, col3 = st.columns([1, 2, 1])
 
