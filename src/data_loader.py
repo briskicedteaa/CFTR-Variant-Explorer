2637b4bff99d3a29785e875a845cf1edb0027d17
ericka <kkikipie@gmail.com>
2026-08-27 23:14:20 -0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -6,6 +6,6 @@ DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 def load_data():
     position_df = pd.read_csv(DATA_DIR / "FinalPosition.csv")
     variants_df = pd.read_csv(DATA_DIR / "CFTR_variants.csv")
-    cftr_df = pd.read_csv(DATA_DIR / "CFTR_DF.csv")
+    cftr_df = pd.read_csv(DATA_DIR / "CFTR_df.csv")
 
     return position_df, variants_df, cftr_df
