6709aec015567f284b167bc5631f29d9924c4eee
ericka <kkikipie@gmail.com>
2026-08-27 22:03:43 -0400

    loaded CFTR_df.csv

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 from pathlib import Path
 
 DATA_DIR = Path(__file__).resolve().parent.parent / "data"
@@ -6,5 +6,6 @@ DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 def load_data():
     position_df = pd.read_csv(DATA_DIR / "FinalPosition.csv")
     variants_df = pd.read_csv(DATA_DIR / "CFTR_variants.csv")
-    
-    return position_df, variants_df
+    cftr_df = pd.read_csv(DATA_DIR / "CFTR_DF.csv")
+
+    return position_df, variants_df, cftr_df
