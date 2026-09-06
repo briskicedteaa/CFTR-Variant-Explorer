066899156a6b2847455070c4d8426eb22578a93a
ericka <kkikipie@gmail.com>
2026-09-06 10:27:23 -0400

    Only showing confidence explanation for other AA positions

--- src/app.py
+++ src/app.py
@@ -425,25 +425,26 @@ if st.session_state["explored_position"] is not None:
                     "No standard amino-acid substitutions are available "
                     "for prediction at this position."
                 )
-                    
-        with st.expander("What does confidence mean?"):
-            st.write(
-                "The confidence score indicates "
-                "how strongly the model favors its prediction. Because the model was trained "
-                "on existing CFTR variant data, its predictions should be interpreted as "
-                "computational estimates rather than definitive evidence of biological or "
-                "clinical effect." 
-            )
-
-            N_PATH = Path(__file__).resolve().parent.parent / "images" / "bcb43178-c776-4fed-b8ee-5b9f36f8bfa7_removalai_preview.png"
-
-            left, center, right = st.columns([1, 3, 1])
-
-            with center:
-                st.image(
-                    str(N_PATH),
-                    use_container_width=True
+                
+        if explored_position != 1481:
+            with st.expander("What confidence means"):
+                st.write(
+                    "The confidence score indicates "
+                    "how strongly the model favors its prediction. Because the model was trained "
+                    "on existing CFTR variant data, its predictions should be interpreted as "
+                    "computational estimates rather than definitive evidence of biological or "
+                    "clinical effect." 
                 )
+
+                N_PATH = Path(__file__).resolve().parent.parent / "images" / "bcb43178-c776-4fed-b8ee-5b9f36f8bfa7_removalai_preview.png"
+
+                left, center, right = st.columns([1, 3, 1])
+
+                with center:
+                    st.image(
+                        str(N_PATH),
+                        use_container_width=True
+                    )
         
         if variants is not None and not variants.empty:
 
