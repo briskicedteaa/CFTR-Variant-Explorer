c9bf6b3508d011f1416499f58c4ef1ddc607e775
ericka <kkikipie@gmail.com>
2026-09-06 16:53:26 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -327,16 +327,6 @@ if st.session_state["explored_position"] is not None:
     else:
         position_info, variants = get_position_summary(explored_position)
         
-        N_PATH = Path(__file__).resolve().parent.parent / "images" / "bcb43178-c776-4fed-b8ee-5b9f36f8bfa7_removalai_preview.png"
-        
-        left, center, right = st.columns([1, 3, 1])
-
-        with center:
-            st.image(
-                str(N_PATH),
-                use_container_width=True
-            )
-        
         st.markdown(
             "<h1 style='text-align: center;'>Results</h1>",
             unsafe_allow_html=True
