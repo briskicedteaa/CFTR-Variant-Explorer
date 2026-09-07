eddd7dbdb9d9e4b5cfa4e90e3db66c5a8bd47198
ericka <kkikipie@gmail.com>
2026-09-07 17:57:26 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -590,16 +590,6 @@ if st.session_state["explored_position"] is not None:
             ].copy()
                     
         else:
-            N_PATH = Path(__file__).resolve().parent.parent / "images" / "bcb43178-c776-4fed-b8ee-5b9f36f8bfa7_removalai_preview.png"
-
-            left, center, right = st.columns([1, 3, 1])
-            
-            with center:
-                st.image(
-                    str(N_PATH),
-                    use_container_width=True
-            )
-                
             st.markdown(
                 f"""
                 <h3 style='text-align: center;'>
