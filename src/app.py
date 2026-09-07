2d3c29d6745268c3642926adcfb251dd2804dbcf
ericka <kkikipie@gmail.com>
2026-09-06 21:39:29 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -509,15 +509,6 @@ if st.session_state["explored_position"] is not None:
                 )
         
         if explored_position != 1481:
-            
-            with st.expander("Model Performance"):
-                col1, col2 = st.columns(2)
-            
-                with col1:
-                    st.metric("Model accuracy", "96.9%")
-            
-                with col2:
-                    st.metric("Macro F1", "55.8%")
                 
             st.markdown(
                 "<h3 style='text-align: center;'>Machine Learning Prediction</h3>",
@@ -579,7 +570,16 @@ if st.session_state["explored_position"] is not None:
                     "Recorded consequence",
                     selected_variant["Consequence"].title()
                 )
-
+                
+        
+                col1, col2 = st.columns(2)
+            
+                with col1:
+                    st.metric("Model accuracy", "96.9%")
+            
+                with col2:
+                    st.metric("Macro F1", "55.8%")
+    
                 if result["confidence"] is not None:
 
                     col3.metric(
