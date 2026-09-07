9e0032532c443dc6391cfbadd73153bbae6a1c6a
ericka <kkikipie@gmail.com>
2026-09-07 17:20:06 -0400

    Made image smaller

--- src/app.py
+++ src/app.py
@@ -483,7 +483,7 @@ if st.session_state["explored_position"] is not None:
         
         LOL_PATH = Path(__file__).resolve().parent.parent / "images" / "Untitled16_20260826211636.png"
 
-        left, center, right = st.columns([1, 3, 1])
+        left, center, right = st.columns([1, 2, 1])
 
         with center:
             st.image(
