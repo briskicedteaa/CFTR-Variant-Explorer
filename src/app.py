41e4e81f07f84d5605d1075129fa5fe90d049a4f
ericka <kkikipie@gmail.com>
2026-09-07 18:57:28 -0400

    Update GIF path in app.py

--- src/app.py
+++ src/app.py
@@ -474,7 +474,7 @@ if st.session_state["explored_position"] is not None:
             unsafe_allow_html=True
         )
         
-        LOL_PATH = Path(__file__).resolve().parent.parent / "images" / "A85ED006-0868-4ADA-9966-0377253351CC.gif"
+        LOL_PATH = Path(__file__).resolve().parent.parent / "images" / "7C388663-A094-40F7-A22D-127FC2BC746B.gif"
 
         left, center, right = st.columns([1, 1, 1])
 
