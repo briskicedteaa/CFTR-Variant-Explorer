89b1f813565e8a9b92fd409599104873f5087c78
ericka <kkikipie@gmail.com>
2026-09-07 18:12:16 -0400

    Fix HL_PATH to reference the correct image filename

--- src/app.py
+++ src/app.py
@@ -174,7 +174,7 @@ human_sequence = cftr_df.loc[
 
 valid_positions = set(variants_df["Position"].dropna().astype(int))
 
-HL_PATH = Path(__file__).resolve().parent.parent / "images" / "images/774121D9-30C7-4AF7-AAEC-F93572DE34E3.gif"
+HL_PATH = Path(__file__).resolve().parent.parent / "images" / "774121D9-30C7-4AF7-AAEC-F93572DE34E3.gif"
 st.image(
     str(HL_PATH),
     use_container_width=True
