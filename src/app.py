d5fa15d9e3da5e7f03fcd52abf65c7edd63e2e25
ericka <kkikipie@gmail.com>
2026-09-13 18:13:19 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -938,7 +938,6 @@ if st.session_state["explored_position"] is not None:
                 name="consequence_selection",
                 fields=["Consequence"],
                 on="click",
-                clear="pointerdown",
                 toggle=False
             )
         
