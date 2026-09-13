9526a2533ad44261f69c54137f4954719874491a
ericka <kkikipie@gmail.com>
2026-09-13 17:32:21 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -938,7 +938,7 @@ if st.session_state["explored_position"] is not None:
                 name="consequence_selection",
                 fields=["Consequence"],
                 on="click",
-                clear=False,
+                clear="mouseup",
                 toggle=False
             )
         
