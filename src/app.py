37b01b106b2dbd94198627e27927af247ac2ab5f
ericka <kkikipie@gmail.com>
2026-09-17 12:42:58 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -116,6 +116,17 @@ h1, h2, h3 {
     text-align: center;
 }
 
+.position-1481-dropdown summary {
+    cursor: pointer;
+    list-style: none;
+    font-family: 'Fredoka', sans-serif;
+    font-size: 2rem;
+    font-weight: 500;
+    color: inherit;
+    text-decoration: underline dotted;
+    text-underline-offset: 3px;
+}
+
 .position-1481-dropdown summary::-webkit-details-marker {
     display: none;
 }
