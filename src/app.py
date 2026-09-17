67856d9aae67de20b2914741d0064f19e121ef72
ericka <kkikipie@gmail.com>
2026-09-17 09:26:41 -0400

    Update app.py

--- src/app.py
+++ src/app.py
@@ -936,7 +936,8 @@ if st.session_state["explored_position"] is not None:
         
             consequence_selection = alt.selection_point(
                 name="consequence_selection",
-                fields=["Consequence"]
+                fields=["Consequence"],
+                clear="mouseup"
             )
             
             consequence_chart = (
